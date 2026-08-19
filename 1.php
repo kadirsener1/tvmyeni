40c260435deed91a50c1d4b3d09fe1fcead40257
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2026-08-19 07:34:01 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -7,7 +7,7 @@
 
 </head><body>
 <video id="hls-example" class="video-js vjs-default-skin" controls preload="auto">
-<source type="application/x-mpegURL" src="http://cdn.vizitv.workers.dev/52.m3u8">
+<source type="application/x-mpegURL" src="https://live.artidijitalmedya.com/artidijital_ciftcitv/ciftcitv/playlist.m3u8">
 </video>
 </body>
 <script src="https://livetv.work/play/iframe-css/video.js"></script> 
@@ -19,3 +19,5 @@ if(videoPlayer != null){
   player.play();
 }
 </script>
+<link href="https://vjs.zencdn.net/8.6.0/video-js.css" rel="stylesheet">
+<script src="https://vjs.zencdn.net/8.6.0/video.min.js"></script>
